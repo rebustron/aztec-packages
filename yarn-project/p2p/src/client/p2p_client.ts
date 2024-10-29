14d7725dbeb9f5e0789d408f512f262fd9679976
Alex Gherghisan <alexg@aztecprotocol.com>
2024-10-29 16:35:54 +0000

    fix: expose env var for batch size

--- yarn-project/p2p/src/client/p2p_client.ts
+++ yarn-project/p2p/src/client/p2p_client.ts
@@ -216,10 +216,10 @@ export class P2PClient extends WithTracer implements P2P {
   ) {
     super(telemetryClient, 'P2PClient');
 
-    const { blockCheckIntervalMS, l2QueueSize } = getP2PConfigEnvVars();
+    const { blockCheckIntervalMS, blockRequestBatchSize } = getP2PConfigEnvVars();
 
     this.blockStream = new L2BlockStream(l2BlockSource, this, this, {
-      batchSize: l2QueueSize,
+      batchSize: blockRequestBatchSize,
       pollIntervalMS: blockCheckIntervalMS,
     });
 
@@ -610,7 +610,6 @@ export class P2PClient extends WithTracer implements P2P {
     const txsToDelete: TxHash[] = [];
     for (const tx of this.txPool.getAllTxs()) {
       // every tx that's been generated against a block that has now been pruned is no longer valid
-      // NOTE (alexg): I think this check against block hash instead of block number?
       if (tx.data.constants.globalVariables.blockNumber.toNumber() > latestBlock) {
         txsToDelete.push(tx.getTxHash());
       }
